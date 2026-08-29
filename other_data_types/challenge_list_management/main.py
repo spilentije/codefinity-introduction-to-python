863148ee89cdaf4b314a124ae9b4376372969a8d
spilentije <>
2026-08-29 09:54:14 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,11 @@
+meat=["Ham", 3.99, 100, "Sliced"]
+cheese=["Cheddar", 5.49, 100, "Sharp"]
+condiment=["Mustard", 1.99, 75, "Spicy"]
+deli_dept=[meat, cheese, condiment]
+print(f"Initial Deli list:", deli_dept)
+seasonal_meat=["Turkey", 4.50, 100, "Sliced"]
+deli_dept.append(seasonal_meat)
+deli_dept.remove(condiment)
+deli_dept.sort()
+print(f"Updated Deli list:", deli_dept)
+
