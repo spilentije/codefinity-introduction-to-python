879f1ca7d8c9e5bdc9c5552bd3419ef1aaac127f
spilentije <>
2026-08-29 17:20:07 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,8 @@
+start_number = 5
+countdown_values = []
+current_number = start_number
+while current_number > 0:
+    countdown_values.append(current_number)
+    current_number -= 1
+print("Discount down complete!")
+print("Countdown complete!", countdown_values)
